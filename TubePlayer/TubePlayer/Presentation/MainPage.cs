492b938059bbcf46e9dfc604998c1dc22e7a220b
sadicangel <sadicangel@hotmail.com>
2023-11-04 16:38:04 +0000

    Create static UI and cleanup main model.

--- TubePlayer/TubePlayer/Presentation/MainPage.cs
+++ TubePlayer/TubePlayer/Presentation/MainPage.cs
@@ -1,30 +1,26 @@
-﻿namespace TubePlayer.Presentation;
+namespace TubePlayer.Presentation;
 
 public sealed partial class MainPage : Page
 {
     public MainPage()
     {
-        this.DataContext<BindableMainModel>((page, vm) => page
-            .NavigationCacheMode(NavigationCacheMode.Required)
+        this.NavigationCacheMode(NavigationCacheMode.Required)
             .Background(Theme.Brushes.Background.Default)
-            .Content(new Grid()
+            .Content(
+                new Grid()
                 .SafeArea(SafeArea.InsetMask.All)
-                .RowDefinitions<Grid>("Auto,*")
+                .RowDefinitions("Auto, *")
                 .Children(
-                    new NavigationBar().Content(() => vm.Title),
-                    new StackPanel()
-                        .Grid(row: 1)
-                        .HorizontalAlignment(HorizontalAlignment.Center)
-                        .VerticalAlignment(VerticalAlignment.Center)
-                        .Spacing(16)
-                        .Children(
-                            new TextBox()
-                                .Text(x => x.Bind(() => vm.Name).Mode(BindingMode.TwoWay))
-                                .PlaceholderText("Enter your name:"),
-                            new Button()
-                                .Content("Go to Second Page")
-                                .AutomationProperties(automationId: "SecondPageButton")
-                                .Command(() => vm.GoToSecond)
-                                ))));
+                    new TextBox()
+                    .PlaceholderText("Search term"),
+                    new ListView()
+                    .Grid(row: 1)
+                    .ItemsSource(new[] { "Avatar", "Titanic", "Star Wars" })
+                    .ItemTemplate<string>(title =>
+                        new TextBlock()
+                        .Text(() => title)
+                    )
+                )
+            );
     }
 }
